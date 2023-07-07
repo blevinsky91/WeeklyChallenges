2d7a0bdfd1f03bd7f4a6ff03247e2eaad18b01b4
Brandon Levinsky <blevinsky91@gmail.com>
2023-07-07 11:04:36 -0400

    weekly challenges2 complete

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -7,52 +7,126 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            var letters = "abcdefghijklmnopqrstuvwxyz";
+            if (letters.Contains(Char.ToLower(c)))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            var count = vals.Length;
+            if (count % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            return number % 2 == 0;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            return num % 2 != 0;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+
+            foreach (double number in numbers)
+            {
+                
+                if (number > max)
+                {
+                    max = number;
+                }
+                if (number < min)
+                {
+                    min = number;
+                }
+                
+            }
+            return max + min;
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length > str2.Length)
+            {
+                return str2.Length;
+            }
+            else
+            {
+                return str1.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null)
+            {
+                return 0;
+            }
+            int result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result += numbers[i];
+            }
+            return answer;
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            int result = 0;
+            foreach (int item in numbers)
+            {
+                if (item % 2 == 0)
+                {
+                    result += item;
+                }
+            }
+            return result;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            int result = 0;
+            foreach (int number in numbers)
+            {
+                result += number;
+            }
+            return (sum % 2 != 0);
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            return number < 0 ? 0 : number / 2;
         }
     }
 }
